ac6263d2aad3392e64f574e14da8875fbe4f1214
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-05 00:06:44 +1100

    docs: Set theme color in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-pink-100': '#FE2D55',
+        'theme-pink-200': '#EF2B50',
+      },
+    },
   },
   plugins: [],
 };
