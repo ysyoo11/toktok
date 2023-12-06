fbada2673dedaa210707ee0a77fcb8d8e50be725
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-06 22:18:49 +1100

    refactor: Refactor by creating ENV object managing env variables

---
+++ src/utils/env.ts
@@ -0,0 +1,27 @@
+const ENV_LIST = [
+  'NEXT_PUBLIC_SANITY_PROJECT_ID',
+  'NEXT_PUBLIC_SANITY_DATASET',
+  'NEXTAUTH_URL',
+  'NEXTAUTH_SECRET',
+  'GOOGLE_CLIENT_ID',
+  'GOOGLE_CLIENT_SECRET',
+  'SANITY_API_TOKEN',
+] as const;
+
+export const ENV = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+} as { [key in (typeof ENV_LIST)[number]]: string };
+
+export const isDev = process.env.NODE_ENV === 'development';
+export const isProd = process.env.NODE_ENV === 'production';
+export const isTest = process.env.NODE_ENV === 'test';
+
+export const baseUrl = isDev
+  ? 'http://localhost:3000'
+  : 'https://toktok-two.vercel.app';
