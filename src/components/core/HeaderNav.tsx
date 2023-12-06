d4acd59af6a71079b6738e10bc8cc8b6b99f5184
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-06 22:25:07 +1100

    feat: Differentiate UI on HeaderNav depending on user authentication status

--- src/components/core/HeaderNav.tsx
+++ src/components/core/HeaderNav.tsx
@@ -10,9 +10,13 @@ import { useState } from 'react';
 import LoginModal from '@/components/LoginModal';
 import MenuButton from '@/components/MenuButton';
 import Button from '@/components/ui/Button';
+import { useUser } from '@/hooks/use-user';
+
+import ProfileButton from '../ProfileButton';
 
 export default function HeaderNav() {
   const [showModal, setShowModal] = useState(false);
+  const { user } = useUser();
 
   return (
     <>
@@ -21,10 +25,16 @@ export default function HeaderNav() {
           <VideoCameraIcon className='h-6 w-6 stroke-2' />
         </Link>
         <Link href='/message'>
-          <PaperAirplaneIcon className='h-6 w-6 -rotate-45 stroke-2' />
+          <PaperAirplaneIcon className='h-6 w-6 -translate-y-0.5 -rotate-45 stroke-2' />
         </Link>
-        <Button onClick={() => setShowModal(true)}>Log in</Button>
-        <MenuButton />
+        {user ? (
+          <ProfileButton image={user.imageURL} name={user.name} />
+        ) : (
+          <>
+            <Button onClick={() => setShowModal(true)}>Log in</Button>
+            <MenuButton />
+          </>
+        )}
       </nav>
       <LoginModal isOpen={showModal} onClose={() => setShowModal(false)} />
     </>
