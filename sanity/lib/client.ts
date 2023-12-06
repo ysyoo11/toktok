745fab90480ad70540d74787d6f35a2db869b5fb
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-06 22:20:56 +1100

    feat: Create getUserByEmail and createUser in sanity util

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,13 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { ENV } from '@/utils/env';
+
+import { apiVersion, dataset, projectId, useCdn } from '../env';
 
 export const client = createClient({
   apiVersion,
   dataset,
   projectId,
   useCdn,
-})
+  token: ENV.SANITY_API_TOKEN,
+});
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,8 +1,13 @@
+import { User as NextAuthUser } from 'next-auth';
+import { AdapterUser } from 'next-auth/adapters';
 import { groq } from 'next-sanity';
 
 import { User } from '@/types';
+import { slugify } from '@/utils/slugify';
+import { urlToObject } from '@/utils/url-to-object';
 
 import { client } from './lib/client';
+import { urlForImage } from './lib/image';
 
 export async function getUsers(): Promise<User[]> {
   return await client.fetch(
@@ -28,10 +33,75 @@ export async function getUserById(id: string): Promise<User> {
     .fetch(
       groq`*[_id == '${id}']{
       name,
+      videos,
+      imageURL,
       following,
-      follower,
-      videos
+      follower
     }`,
     )
     .then((res) => res[0]);
+}
+
+export async function getUserByEmail(email: string): Promise<User> {
+  return await client
+    .fetch(
+      groq`*[_type=='user' && email=='${email}']{
+      _id,
+      name,
+      videos,
+      imageURL,
+      following,
+      follower
+    }`,
+    )
+    .then((res) => res[0]);
+}
+
+async function uploadImage(file: File, username: string) {
+  return await client.assets.upload('image', file, {
+    contentType: 'image/jpg',
+    filename: `${username}.jpg`,
+  });
+}
+
+export async function createUser(user: NextAuthUser | AdapterUser) {
+  const { image, id, name, email } = user;
+
+  const file = image ? await urlToObject(image) : null;
+
+  let imageId = '';
+
+  if (file) {
+    await uploadImage(file, name || 'image')
+      .then((doc) => {
+        imageId = doc._id;
+      })
+      .catch(console.error);
+  }
+
+  const asset = {
+    _type: 'reference',
+    _ref: imageId,
+  };
+
+  const userData = {
+    _type: 'user',
+    _id: id,
+    name,
+    email,
+    slug: {
+      _type: 'slug',
+      current: slugify(email!.split('@')[0]),
+    },
+    image: file
+      ? {
+          _type: 'image',
+          asset,
+          alt: `Profile of ${name}`,
+        }
+      : null,
+    imageURL: file ? urlForImage({ asset }).url() : '',
+  };
+
+  return await client.createIfNotExists(userData).catch(console.error);
 }
