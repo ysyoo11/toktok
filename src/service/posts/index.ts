c65d89d033e9141f91c89f16b48f37ff0c5b5548
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-14 06:16:11 +1100

    feat: Create api for getting video by id

--- src/service/posts/index.ts
+++ src/service/posts/index.ts
@@ -3,11 +3,17 @@ import { Video } from '@/types';
 import { baseUrl } from '@/utils/env';
 
 // TODO: Make a logic for infinite scroll
-export async function getAllVideos(): Promise<Video[]> {
-  const { videos } = await fetch(baseUrl + '/api/videos') //
+export async function getAllVideos(username?: string): Promise<Video[]> {
+  const { videos } = await fetch(baseUrl + `/api/posts?username=${username}`) //
     .then((res) => res.json());
 
   return videos;
+}
+
+export async function getVideoById(videoId: string): Promise<Video> {
+  const { video } = await fetch(`/api/posts/${videoId}`) //
+    .then((res) => res.json());
+  return video;
 }
 
 export async function createVideo(form: VideoPostForm) {
@@ -15,7 +21,7 @@ export async function createVideo(form: VideoPostForm) {
   Object.entries(form) //
     .forEach((item) => formData.append(item[0], item[1]!));
 
-  return await fetch('/api/videos', {
+  return await fetch('/api/posts', {
     method: 'POST',
     cache: 'no-cache',
     body: formData,
