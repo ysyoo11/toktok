5c1ab82211287c97860f5f951a335bc15114c17a
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-14 06:22:56 +1100

    feat: Create AuthLayout

---
+++ src/app/signin/layout.tsx
@@ -0,0 +1,18 @@
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+
+import { authOptions } from '../api/auth/[...nextauth]/options';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default async function AuthLayout({ children }: Props) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (user) {
+    redirect('/');
+  }
+
+  return <div>{children}</div>;
+}
