9e138d365d07ccbc4b92531f578e84ee5c4706ec
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-13 20:58:35 +1100

    feat: Create RangeController component

---
+++ src/components/ui/RangeController.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import { ChangeEvent } from 'react';
+
+type Props = {
+  className?: string;
+  value: number;
+  max: number;
+  min: number;
+  step?: number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function RangeController({
+  className,
+  value,
+  max,
+  min,
+  onChange,
+  step = 1,
+}: Props) {
+  return (
+    <div
+      className={clsx(
+        'group/range relative flex h-[0.125rem] min-w-[2rem] items-center rounded-full bg-white/30 hover:h-1',
+        className,
+      )}
+    >
+      <div
+        className='h-full rounded-full bg-white'
+        style={{ width: `${(value / max) * 100}%` }}
+      />
+      <input
+        type='range'
+        className='custom-range absolute w-full'
+        max={max}
+        min={min}
+        step={step}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
