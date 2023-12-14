b07ac54bf570f8c57a8810d8336a847bb081f979
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-14 22:39:10 +1100

    [WIP] refactor: Refactor PostEngagementBar

--- src/components/PostEngagementBar.tsx
+++ src/components/PostEngagementBar.tsx
@@ -3,93 +3,79 @@ import {
   ChatBubbleOvalLeftEllipsisIcon,
   HeartIcon,
 } from '@heroicons/react/24/solid';
-import clsx from 'clsx';
-import { useMemo } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCallback, useState } from 'react';
 
 import { useUser } from '@/hooks/use-user';
+import { likeVideo } from '@/lib/posts/like';
 
-type Engagement = 'like' | 'comment' | 'save' | 'share';
-type EngagementItem = {
-  type: Engagement;
-  icon: React.ForwardRefExoticComponent<
-    Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
-      title?: string | undefined;
-      titleId?: string | undefined;
-    } & React.RefAttributes<SVGSVGElement>
-  >;
-  onClick: () => void;
-  number: number;
-  isEngaged?: boolean;
-};
+import ToggleButton from './ui/ToggleButton';
+
+import type { SimplePost } from '@/model/post';
 
 type Props = {
-  videoId: string;
-  likesNum: number;
-  commentsNum: number;
-  savedNum: number;
+  post: SimplePost;
 };
 
-export default function PostEngagementBar({
-  videoId,
-  likesNum,
-  commentsNum,
-  savedNum,
-}: Props) {
-  const { uid } = useUser();
+export default function PostEngagementBar({ post }: Props) {
+  const { likes, author, caption, createdAt, id, comments } = post;
 
-  // TODO:
-  const handleLike = () => {};
+  const { uid } = useUser();
+  const router = useRouter();
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+
+  // 1. if user is not logged in, router.push('/signin') ✅
+  // 2. if (!isLiked) add like to the video ✅
+  // 3. if (isLiked) delete like from the video ✅
+  // 4. return `isLikeByUser` from server ✅
+  // 5. mutate like/unlike properly
+
+  const handleLike = useCallback(async () => {
+    if (!uid) return router.push('/signin');
+
+    setIsLiked((prev) => !prev);
+    if (isLiked) {
+      await likeVideo(id, false);
+    } else {
+      await likeVideo(id, true);
+    }
+  }, [uid, router, id, isLiked]);
+
   const handleComment = () => {};
   const handleSave = () => {};
 
-  const engagementList: EngagementItem[] = useMemo(
-    () => [
-      {
-        type: 'like',
-        icon: HeartIcon,
-        onClick: handleLike,
-        number: likesNum,
-        isEngaged: false, // TODO:
-      },
-      {
-        type: 'comment',
-        icon: ChatBubbleOvalLeftEllipsisIcon,
-        onClick: handleComment,
-        number: commentsNum,
-      },
-      {
-        type: 'save',
-        icon: BookmarkIcon,
-        onClick: handleSave,
-        number: savedNum,
-        isEngaged: false, // TODO:
-      },
-    ],
-    [likesNum, commentsNum, savedNum],
-  );
-
   return (
     <ul className='flex flex-col px-4'>
-      {engagementList.map(
-        ({ type, onClick, number, isEngaged, ...item }, idx) => (
-          <li key={`${type}-${idx}`} className='flex flex-col items-center'>
-            <button
-              onClick={onClick}
-              className='rounded-full bg-gray-100 p-1.5 sm:p-2.5'
-            >
-              <item.icon
-                className={clsx('h-5 w-5 sm:h-7 sm:w-7', {
-                  'text-red-500': isEngaged && type === 'like',
-                  'text-yellow-500': isEngaged && type === 'save',
-                })}
-              />
-            </button>
-            <span className='text-sm font-semibold text-gray-600'>
-              {number}
-            </span>
-          </li>
-        ),
-      )}
+      <li className='flex flex-col items-center'>
+        <ToggleButton
+          toggled={isLiked}
+          onToggle={setIsLiked}
+          onIcon={<HeartIcon className='h-5 w-5 text-red-500 sm:h-7 sm:w-7' />}
+          offIcon={<HeartIcon className='h-5 w-5 sm:h-7 sm:w-7' />}
+          wrapped
+        />
+        <span className='text-sm font-semibold text-gray-600'>0</span>
+      </li>
+      <li className='flex flex-col items-center'>
+        <button className='rounded-full bg-gray-100 p-1.5 sm:p-2.5'>
+          <ChatBubbleOvalLeftEllipsisIcon className='h-5 w-5 sm:h-7 sm:w-7' />
+        </button>
+        <span className='text-sm font-semibold text-gray-600'>{comments}</span>
+      </li>
+      <li className='flex flex-col items-center'>
+        <ToggleButton
+          toggled={isSaved}
+          onToggle={setIsSaved}
+          onIcon={
+            <BookmarkIcon className='h-5 w-5 text-yellow-500 sm:h-7 sm:w-7' />
+          }
+          offIcon={<BookmarkIcon className='h-5 w-5 sm:h-7 sm:w-7' />}
+          wrapped
+        />
+        <span className='text-sm font-semibold text-gray-600'>0</span>
+      </li>
     </ul>
   );
 }
---
+++ src/swr.ts
@@ -0,0 +1,3 @@
+export const VIDEO_SWR_KEY = {
+  GET_VIDEO_BY_ID: '@get-video-by-id',
+};
