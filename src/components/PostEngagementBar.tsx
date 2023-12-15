a8a0307731382a37541984303eaa5e8b5e75d057
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-16 05:44:49 +1100

    feat: Like feature

--- src/components/PostEngagementBar.tsx
+++ src/components/PostEngagementBar.tsx
@@ -4,10 +4,10 @@ import {
   HeartIcon,
 } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/navigation';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
+import usePosts from '@/hooks/use-posts';
 import { useUser } from '@/hooks/use-user';
-import { likeVideo } from '@/lib/posts/like';
 
 import ToggleButton from './ui/ToggleButton';
 
@@ -18,45 +18,42 @@ type Props = {
 };
 
 export default function PostEngagementBar({ post }: Props) {
-  const { likes, author, caption, createdAt, id, comments } = post;
+  const { likes, comments } = post;
 
-  const { uid } = useUser();
+  const { user } = useUser();
+  const liked = user ? likes.includes(user.username) : false;
   const router = useRouter();
 
-  const [isLiked, setIsLiked] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
 
-  // 1. if user is not logged in, router.push('/signin') ✅
-  // 2. if (!isLiked) add like to the video ✅
-  // 3. if (isLiked) delete like from the video ✅
-  // 4. return `isLikeByUser` from server ✅
-  // 5. mutate like/unlike properly
+  const { setLike } = usePosts();
 
-  const handleLike = useCallback(async () => {
-    if (!uid) return router.push('/signin');
+  const handleLike = async (isLiked: boolean) => {
+    if (!user) return router.push('/signin');
 
-    setIsLiked((prev) => !prev);
-    if (isLiked) {
-      await likeVideo(id, false);
-    } else {
-      await likeVideo(id, true);
-    }
-  }, [uid, router, id, isLiked]);
+    setLike(post, user.username, isLiked);
+  };
 
-  const handleComment = () => {};
+  const handleComment = () => {
+    // TODO: open post detail modal
+  };
+
+  // TODO:
   const handleSave = () => {};
 
   return (
     <ul className='flex flex-col px-4'>
       <li className='flex flex-col items-center'>
         <ToggleButton
-          toggled={isLiked}
-          onToggle={setIsLiked}
+          toggled={liked}
+          onToggle={handleLike}
           onIcon={<HeartIcon className='h-5 w-5 text-red-500 sm:h-7 sm:w-7' />}
           offIcon={<HeartIcon className='h-5 w-5 sm:h-7 sm:w-7' />}
           wrapped
         />
-        <span className='text-sm font-semibold text-gray-600'>0</span>
+        <span className='text-sm font-semibold text-gray-600'>
+          {likes.length}
+        </span>
       </li>
       <li className='flex flex-col items-center'>
         <button className='rounded-full bg-gray-100 p-1.5 sm:p-2.5'>
