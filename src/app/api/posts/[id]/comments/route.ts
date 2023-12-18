45052e79939bb6ed318c421739889078509db7c9
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-18 16:27:23 +1100

    feat: Create api for getting comments

---
+++ src/app/api/posts/[id]/comments/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { getComments } from '@/service/posts/comments';
+
+export async function GET(
+  _req: NextRequest,
+  { params: { id } }: { params: { id: string } },
+) {
+  const comments = await getComments(id);
+
+  return NextResponse.json(comments, { status: 200 });
+}
---
+++ src/service/posts/comments.ts
@@ -0,0 +1,45 @@
+import { groq } from 'next-sanity';
+
+import { Comment } from '@/model/post';
+
+import { client } from '../sanity';
+
+// TODO:
+// 1. fetch 12 comments each time (infinite scroll)
+// 2. fetch only the first reply and fetch 3 more when requested to show more replies
+export async function getComments(postId: string) {
+  const comments = await client
+    .fetch<{ comments: Comment[] }>(
+      groq`*[_type == 'video' && _id == '${postId}'][0]{
+    "comments": comments[]{
+      "key": _key,
+      "authorUsername": author->username,
+      "authorImage": author->imageUrl,
+      text,
+      "likes": likes[]->username,
+      "replies": replies[]{
+        "key": _key,
+        "authorUsername": author->username,
+        "authorImage": author->imageUrl,
+        "likes": likes[]->username,
+        text
+      }
+    }
+  }`,
+    )
+    .then((data) => mapComments(data.comments));
+  return comments;
+}
+
+function mapComments(comments: Comment[]) {
+  return comments.map((comment) => ({
+    ...comment,
+    likes: comment.likes ?? [],
+    replies: comment.replies
+      ? comment.replies.map((reply) => ({
+          ...reply,
+          likes: reply.likes ?? [],
+        }))
+      : [],
+  }));
+}
