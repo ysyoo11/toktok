e0fe0d4cc98c068402e5f3af3b2d6663993afb3c
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-18 16:29:56 +1100

    feat: Create PostComment and PostCommentReply components

---
+++ src/components/PostComment.tsx
@@ -0,0 +1,74 @@
+import { HeartIcon } from '@heroicons/react/24/outline';
+import { HeartIcon as SolidHeartIcon } from '@heroicons/react/24/solid';
+import { usePathname, useRouter } from 'next/navigation';
+
+import { useUser } from '@/hooks/use-user';
+import useComments from '@/hooks/useComments';
+
+import Avatar from './Avatar';
+import CommentReply from './PostCommentReply';
+import ToggleButton from './ui/ToggleButton';
+
+import type { Comment } from '@/model/post';
+
+type Props = {
+  comment: Comment;
+};
+
+export default function PostComment({
+  comment: { authorImage, authorUsername, text, likes, replies },
+}: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const postId = pathname.split('/post/')[1];
+  const { setLike } = useComments(postId);
+  const { user } = useUser();
+  const liked = user ? likes.includes(user.username) : false;
+  const writtenByAuthor = authorUsername === user?.username;
+
+  const handleLike = () => {
+    if (!user) router.push('/signin', { scroll: false });
+  };
+
+  return (
+    <>
+      <div className='flex w-full items-center justify-between'>
+        <div className='flex w-full'>
+          <Avatar size='sm' image={authorImage} name={authorUsername} />
+          <div className='ml-2 w-full'>
+            <div className='flex w-full justify-between'>
+              <div>
+                <span className='font-medium text-gray-500'>
+                  {authorUsername}
+                </span>
+                {writtenByAuthor && (
+                  <span>
+                    &nbsp;&middot;&nbsp;
+                    <span className='text-theme-pink-100'>Author</span>
+                  </span>
+                )}
+                <p>{text}</p>
+              </div>
+              <div className='flex items-center space-x-1'>
+                <ToggleButton
+                  toggled={liked}
+                  onToggle={handleLike}
+                  onIcon={<SolidHeartIcon className='h-5 w-5 text-red-500' />}
+                  offIcon={<HeartIcon className='h-5 w-5 text-gray-400' />}
+                />
+                <span className='text-gray-400'>{likes.length}</span>
+              </div>
+            </div>
+            {replies.length > 0 && (
+              <ul className='mt-4'>
+                {replies.map((reply) => (
+                  <CommentReply key={reply.key} reply={reply} />
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/PostCommentReply.tsx
@@ -0,0 +1,30 @@
+import { useUser } from '@/hooks/use-user';
+
+import Avatar from './Avatar';
+
+import type { Reply } from '@/model/post';
+
+type Props = {
+  reply: Reply;
+};
+
+export default function PostCommentReply({
+  reply: { authorImage, authorUsername, text },
+}: Props) {
+  const { user } = useUser();
+  return (
+    <div className='flex'>
+      <Avatar size='xs' image={authorImage} name={authorUsername} />
+      <div className='ml-2'>
+        <span className='font-medium text-gray-500'>{authorUsername}</span>
+        {authorUsername === user?.username && (
+          <span>
+            &nbsp;&middot;&nbsp;
+            <span className='text-theme-pink-100'>Author</span>
+          </span>
+        )}
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+}
