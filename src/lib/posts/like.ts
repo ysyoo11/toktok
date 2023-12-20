b9698b37d4878611ace91e925cba83ef5b8c9f5b
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-21 05:25:37 +1100

    feat: Create backend logic for reply like/dislike

--- src/lib/posts/like.ts
+++ src/lib/posts/like.ts
@@ -1,4 +1,4 @@
-export async function updateLike(postId: string, like: boolean) {
+export async function updatePostLike(postId: string, like: boolean) {
   return await fetch(`/api/posts/${postId}/likes`, {
     method: 'PUT',
     headers: {
@@ -20,4 +20,25 @@ export async function updateCommentLike(
     },
     body: JSON.stringify({ commentKey, like }),
   }).then((res) => res.json());
+}
+
+type ReplyLikeProps = {
+  postId: string;
+  commentKey: string;
+  replyKey: string;
+  like: boolean;
+};
+export async function updateReplyLike({
+  postId,
+  commentKey,
+  replyKey,
+  like,
+}: ReplyLikeProps) {
+  return await fetch(
+    `/api/posts/${postId}/comments/${commentKey}/replies/likes`,
+    {
+      method: 'PUT',
+      body: JSON.stringify({ replyKey, like }),
+    },
+  ).then((res) => res.json());
 }
