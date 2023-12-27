25dfc5a390d8dd0095cf4b01368f0d106b7203f6
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-27 21:52:59 +1100

    fix: Re-make useComments hook to fix mutation bug

--- src/hooks/useComments.ts
+++ src/hooks/useComments.ts
@@ -1,10 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
-import { Arguments, mutate as globalMutate } from 'swr';
-import useSWRInfinite from 'swr/infinite';
+import { Arguments, useSWRConfig } from 'swr';
+import useSWRImmutable from 'swr/immutable';
 
 import { getComments, postComment } from '@/lib/posts/comments';
 import { updateCommentLike } from '@/lib/posts/like';
-import { User } from '@/model/user';
 import { POLICY } from '@/policy';
 import { VIDEO_SWR_KEY } from '@/swr';
 
@@ -12,41 +11,30 @@ import type { Comment, SimplePost } from '@/model/post';
 
 export default function useComments(post: SimplePost) {
   const COMMENT_SWR_BASE_KEY = `${VIDEO_SWR_KEY.GET_POST_COMMENTS}-${post.id}`;
-
+  const { mutate: globalMutate } = useSWRConfig();
   const [comments, setComments] = useState<Comment[]>([]);
+  const [page, setPage] = useState(1);
   const lastCommentDateRef = useRef('0');
+  const addCommentFlagRef = useRef(false);
+  const maxCommentNumRef = useRef(0);
 
-  const { data, size, setSize, isLoading, mutate } = useSWRInfinite(
-    (index) => `${COMMENT_SWR_BASE_KEY}-${index}`,
-    async () => {
-      const data = await getComments({
+  const { data, isLoading } = useSWRImmutable(
+    `${COMMENT_SWR_BASE_KEY}-${page}`,
+    async () =>
+      await getComments({
         postId: post.id,
         lastCommentDate: lastCommentDateRef.current,
-      });
-      return data;
-    },
-    {
-      revalidateFirstPage: false,
-      revalidateIfStale: false,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    },
+      }),
   );
 
   const loadMore = () => {
-    setSize((prev) => prev + 1);
+    setPage((prev) => prev + 1);
   };
 
-  useEffect(() => {
-    if (comments.length > 0) {
-      lastCommentDateRef.current = comments[comments.length - 1].createdAt;
-    } else {
-      lastCommentDateRef.current = '0';
-    }
-  }, [comments]);
-
   const isReachingEnd =
-    comments && comments.length < size * POLICY.COMMENT_FETCH_LIMIT;
+    comments.length === 0 ||
+    (comments.length < page * POLICY.COMMENT_FETCH_LIMIT &&
+      maxCommentNumRef.current <= comments.length);
 
   const setLike = async (comment: Comment, username: string, like: boolean) => {
     const newComment = {
@@ -56,7 +44,7 @@ export default function useComments(post: SimplePost) {
         : comment.likes.filter((item) => item !== username),
     };
     const newComments = comments?.map((c) =>
-      c.key === comment.key ? newComment : c,
+      c.id === comment.id ? newComment : c,
     );
     setComments(newComments);
 
@@ -72,39 +60,49 @@ export default function useComments(post: SimplePost) {
     );
   };
 
-  // TODO: Refactor
-  const addComment = async (user: User, comment: string) => {
+  const addComment = async (comment: string) => {
     const newPost = { ...post, comments: post.comments + 1 };
-
     globalMutate(`/api/posts/${post.id}`, true, {
       optimisticData: newPost,
       revalidate: false,
       populateCache: false,
       rollbackOnError: false,
     });
-
-    return await mutate(postComment({ postId: post.id, comment }), {
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    }).then((res: any) => {
-      if (!res || !user) return;
-      const comments = res.data.comments as Comment[];
-      const uploadedComment = comments[comments.length - 1];
-      const modifiedComment: Comment = {
-        ...uploadedComment,
-        authorImage: user.imageUrl,
-        authorUsername: user.username,
-      };
-      setComments((prev) => [...prev, modifiedComment]);
-    });
+    addCommentFlagRef.current = true;
+    await postComment({ postId: post.id, comment }) //
+      .then(() => {
+        lastCommentDateRef.current = '0';
+        globalMutate(
+          (key) =>
+            typeof key === 'string' && key.startsWith(COMMENT_SWR_BASE_KEY),
+        );
+      });
   };
 
   useEffect(() => {
+    if (comments.length > 0) {
+      lastCommentDateRef.current = comments[comments.length - 1].createdAt;
+    } else {
+      lastCommentDateRef.current = '0';
+    }
+  }, [comments]);
+
+  useEffect(() => {
     if (data) {
-      setComments(data.flat());
+      if (addCommentFlagRef.current === true) {
+        setComments(data);
+      } else {
+        setComments((prev) => [...prev, ...data]);
+      }
+      addCommentFlagRef.current = false;
     }
   }, [data]);
+
+  useEffect(() => {
+    if (maxCommentNumRef.current < comments.length) {
+      maxCommentNumRef.current = comments.length;
+    }
+  }, [comments.length]);
 
   return {
     comments,
@@ -113,5 +111,6 @@ export default function useComments(post: SimplePost) {
     isReachingEnd,
     loadMore,
     addComment,
+    page,
   };
 }
