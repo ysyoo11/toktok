91c31367dfd135cff1ed9abecbf45ace3bb2913e
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-29 23:16:02 +1100

    fix: Fix usePost custom hook

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -3,12 +3,15 @@ import useSWRImmutable from 'swr/immutable';
 import { SimplePost } from '@/model/post';
 
 export default function usePost(postId: string) {
-  const { data: post, isLoading } = useSWRImmutable<SimplePost>(
-    `/api/posts/${postId}`,
-  );
+  const {
+    data: post,
+    isLoading,
+    error,
+  } = useSWRImmutable<SimplePost>(`/api/posts/${postId}`);
 
   return {
     post,
     loading: isLoading,
+    error,
   };
 }
