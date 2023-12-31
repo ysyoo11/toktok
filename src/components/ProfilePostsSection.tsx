c195c6b7d4d599add47ef61794c1205a37b2d8bd
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-31 22:38:47 +1100

    feat: Create ProfilePostCard and ProfilePostsSection component

---
+++ src/components/ProfilePostsSection.tsx
@@ -0,0 +1,45 @@
+import { InView } from 'react-intersection-observer';
+
+import useProfilePosts from '@/hooks/useProfilePosts';
+
+import ProfilePostCard from './ProfilePostCard';
+import Loading from './ui/Loading';
+
+type Props = {
+  username: string;
+};
+
+export default function ProfilePostsSection({ username }: Props) {
+  const { posts, isReachingEnd, loadMore, loading } = useProfilePosts(username);
+
+  if (!posts)
+    return (
+      <div className='py-14'>
+        <Loading className='w-12' />
+      </div>
+    );
+
+  return (
+    <>
+      <ul className='mt-2 grid grid-cols-2 gap-x-4 gap-y-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
+        {posts.map((post) => (
+          <li key={`user-post-${post.id}`}>
+            <ProfilePostCard post={post} />
+          </li>
+        ))}
+      </ul>
+      {loading && <Loading className='w-12 py-20' />}
+      {!isReachingEnd && (
+        <div className='py-2'>
+          <InView
+            as='div'
+            rootMargin='24px'
+            onChange={(inView) => {
+              if (inView) loadMore();
+            }}
+          />
+        </div>
+      )}
+    </>
+  );
+}
