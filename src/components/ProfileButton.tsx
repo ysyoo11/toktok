8aedf75009253dc48f93c568a08b7b6de0495631
Yeonsuk Yoo <yysuk0302@gmail.com>
2023-12-31 22:40:10 +1100

    feat: Add a link to profile page in ProfileButton component

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -4,11 +4,13 @@ import { Menu, Transition } from '@headlessui/react';
 import {
   ArrowRightOnRectangleIcon,
   MoonIcon,
+  UserIcon,
 } from '@heroicons/react/24/outline';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { signOut } from 'next-auth/react';
 import { Fragment } from 'react';
 
+import { useUser } from '@/hooks/useUser';
 import { baseUrl } from '@/utils/env';
 
 import Avatar from './Avatar';
@@ -21,9 +23,19 @@ type Props = {
 
 export default function ProfileButton({ image, name }: Props) {
   const pathname = usePathname();
+  const router = useRouter();
+  const { user } = useUser();
+
+  const menuItems = [
+    {
+      icon: UserIcon,
+      text: 'View profile',
+      onClick: () => router.push(`/${user?.username}`),
+    },
+  ];
 
   const logOut = () => {
-    signOut({ callbackUrl: `${baseUrl}${pathname}` });
+    signOut({ callbackUrl: baseUrl + pathname });
   };
 
   return (
@@ -46,39 +58,47 @@ export default function ProfileButton({ image, name }: Props) {
         leaveFrom='transform opacity-100 scale-100'
         leaveTo='transform opacity-0 scale-95'
       >
-        <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
+        <Menu.Items
+          as='div'
+          className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white p-1 shadow-lg ring-1 ring-black/5 focus:outline-none'
+        >
           <div className='p-1'>
-            <Menu.Item>
-              {({ active }) => (
-                <div
-                  className={`${
-                    active ? 'bg-gray-100' : 'text-gray-900'
-                  } group flex w-full items-center justify-between rounded-md p-3`}
-                >
+            <ul>
+              {menuItems.map(({ onClick, text, ...menu }, idx) => (
+                <Menu.Item as='li' key={`menu-item-${idx}`}>
+                  <button
+                    className='flex w-full items-center justify-between rounded-md p-3 hover:bg-gray-100'
+                    onClick={onClick}
+                  >
+                    <div className='flex items-center space-x-2'>
+                      <menu.icon className='h-5 w-5' />
+                      <span>{text}</span>
+                    </div>
+                  </button>
+                </Menu.Item>
+              ))}
+              <Menu.Item as='li'>
+                <div className='flex w-full items-center justify-between rounded-md p-3 hover:bg-gray-100'>
                   <div className='flex items-center space-x-2'>
                     <MoonIcon className='h-5 w-5' />
                     <span>Dark mode</span>
                   </div>
                   <DarkModeToggle />
                 </div>
-              )}
-            </Menu.Item>
+              </Menu.Item>
+            </ul>
           </div>
           <div className='p-1'>
             <Menu.Item>
-              {({ active }) => (
-                <button
-                  onClick={logOut}
-                  className={`${
-                    active ? 'bg-gray-100' : 'text-gray-900'
-                  } group flex w-full items-center justify-between rounded-md p-3`}
-                >
-                  <div className='flex items-center space-x-2'>
-                    <ArrowRightOnRectangleIcon className='h-5 w-5 stroke-2' />
-                    <span>Sign out</span>
-                  </div>
-                </button>
-              )}
+              <button
+                onClick={logOut}
+                className='flex w-full items-center justify-between rounded-md p-3 hover:bg-gray-100'
+              >
+                <div className='flex items-center space-x-2'>
+                  <ArrowRightOnRectangleIcon className='h-5 w-5 stroke-2' />
+                  <span>Sign out</span>
+                </div>
+              </button>
             </Menu.Item>
           </div>
         </Menu.Items>
