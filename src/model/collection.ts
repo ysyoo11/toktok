ea1a9811e06c48ffec29f471d29c0e6bd3300da7
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-01-02 22:16:47 +1100

    feat: Define Collection and SimpleCollection types

--- src/model/collection.ts
+++ src/model/collection.ts
@@ -1,1 +1,14 @@
-export type Collection = {};
+import { SimplePost } from './post';
+
+export type Collection = {
+  id: string;
+  name: string;
+  posts: SimplePost[];
+};
+
+export type SimpleCollection = {
+  id: string;
+  name: string;
+  firstVideoUrl: string;
+  createdAt: string;
+};
