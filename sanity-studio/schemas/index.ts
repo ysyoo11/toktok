d473393afcefb12355dfa36be2317090d6e868f1
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-01-03 23:20:03 +1100

    refactor: Create Collection schema

--- sanity-studio/schemas/index.ts
+++ sanity-studio/schemas/index.ts
@@ -2,7 +2,8 @@ import {type SchemaTypeDefinition} from 'sanity'
 import user from './user'
 import video from './video'
 import music from './music'
+import collection from './collection'
 
 export const schema: {types: SchemaTypeDefinition[]} = {
-  types: [user, video, music],
+  types: [user, video, music, collection],
 }
